3ecd781e4ad04d2aea761be116ec8b29eb8168d7
manuhatho <amateur9525@gmail.com>
2023-12-07 22:07:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+
+const barMenu = document.querySelector(".menu");
+const navMenu = document.querySelector(".nav2");
+const navMenu2 = document.querySelector(".nav3");
+const header = document.querySelector("header");
+const body = document.querySelector("body");
+document.addEventListener("scroll",function(){
+    if(window.scrollY > 0){
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active")
+    }
+})
+barMenu.addEventListener("click",function(){
+    console.log('click');
+    barMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    navMenu2.classList.toggle('active');
+    body.classList.toggle('active')
+})
+
+
+
+
+const first = document.querySelector('.first');
+const home = document.getElementById('home')
+first.addEventListener("mouseover",function(){
+    home.classList.add('black')
+})
+first.addEventListener("mouseout",function(){
+    home.classList.remove('black')
+})
+
+let imageIndex = 0;
+gallery();
+function plusImage(n){
+    gallery(imageIndex += n)
+}
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+
+next.addEventListener("click",function(){
+    plusImage(1);
+})
+previous.addEventListener("click",function(){
+    plusImage(-1);
+})
+
+next.addEventListener("mousedown",function(){
+    next.style.transform = "scale(0.8)";
+})
+next.addEventListener('mouseup',function(){
+    next.style.transform = "scale(1)";
+})
+previous.addEventListener("mousedown",function(){
+    previous.style.transform = "scale(0.8)";
+})
+previous.addEventListener('mouseup',function(){
+    previous.style.transform = "scale(1)";
+})
+function gallery (){
+   let images = document.querySelectorAll('#image');
+   images.forEach(function(){
+    if(imageIndex < 0){
+        imageIndex = images.length -1;
+    }
+
+    if(imageIndex >= images.length){
+        imageIndex = 0;
+    }
+
+   
+    for(i = 0; i < images.length; i++){
+        images[i].style.display = 'none';
+        
+      
+
+    }
+    
+    images[imageIndex].style.display = 'block';
+   
+   });
+}
+
+ gallery();
+ 
+
+ function diapo (){
+    let images = document.getElementsByClassName("image");
+    
+       for(i = 0 ; i < images.length; i++){
+        images[i].style.display = 'none';
+       }
+       imageIndex++
+       if(imageIndex >= images.length){
+        imageIndex = 0;
+       }
+       images[imageIndex].style.display = "block";
+
+       setTimeout(diapo,15000);
+
+ }
+ diapo();
+
+// -------------------------------------------------------------------
+
+
+ 
